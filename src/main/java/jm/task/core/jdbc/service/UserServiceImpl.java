8a1bc5357e2133718b988fc11a783964858686ce
Kipiru <dtaranets@gmail.com>
2021-10-30 14:42:52 +0300

    added UserDaoJDBCImpl realization

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package jm.task.core.jdbc.service;
 
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
@@ -11,76 +13,32 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     public void createUsersTable() {
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-            statement.execute("CREATE TABLE IF NOT EXISTS users " +
-                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(45), " +
-                    "lastName VARCHAR(45), age TINYINT)");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-            statement.execute(" DROP TABLE IF EXISTS users");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, Byte age) {
-        User user = new User(name, lastName, age);
-        String INSERT_USER = "INSERT INTO schema1_1_3.users(name, lastName, age) VALUES (?, ?, ?)";
-        try (Connection connection = Util.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER)) {
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getLastName());
-            preparedStatement.setByte(3,user.getAge());
-            preparedStatement.executeUpdate();
-            System.out.printf("User с именем – %s добавлен в базу данных\n", user.getName());
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-             statement.execute("DELETE FROM schema1_1_3.users WHERE id = " + id);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-            ResultSet rs = statement.executeQuery("SELECT * FROM schema1_1_3.users");
-            while (rs.next()) {
-                long id = rs.getLong("id");
-                String name = rs.getString("name");
-                String lastName = rs.getString("lastName");
-                byte age = rs.getByte("age");
-                users.add(new User(id, name, lastName, age));
-            }
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return users;
+        UserDao userDao = new UserDaoJDBCImpl();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-            statement.execute("DELETE FROM schema1_1_3.users");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.cleanUsersTable();
     }
 }
